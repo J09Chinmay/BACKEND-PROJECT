386856533c8ac6b04da6060c5f21cce73549fb1e
J09Chinmay <mori09jim@gmail.com>
2023-12-26 11:07:09 +0530

    Index Modified

--- travellerApp/src/main/java/com/traveller/travellerApp/ReversePositiveIntegers.java
+++ travellerApp/src/main/java/com/traveller/travellerApp/ReversePositiveIntegers.java
@@ -1,38 +1,37 @@
 package com.traveller.travellerApp;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class ReversePositiveIntegers {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // ArrayList<Integer> positiveIntegers = new ArrayList<>();
+        try (Scanner scanner = new Scanner(System.in)) {
+            // for (int i = positiveIntegers.size() - 1; i >= 0; i--) {
+            // System.out.print(positiveIntegers.get(i) + " ");
+            // }
+            String s = scanner.nextLine();
+            Stack<Character> stk = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    stk.push(s.charAt(i));
+                } else if (s.charAt(i) == ')') {
+                    if (stk.isEmpty() || stk.peek() != '(') {
+                        return;
+                    }
+                    stk.pop();
+                }
+            }
+            if (stk.isEmpty()) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }
 
         // int num;
         // while ((num = scanner.nextInt()) >= 0) {
         // positiveIntegers.add(num);
         // }
 
-        // for (int i = positiveIntegers.size() - 1; i >= 0; i--) {
-        // System.out.print(positiveIntegers.get(i) + " ");
-        // }
-        String s = scanner.nextLine();
-        Stack<Character> stk = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stk.push(s.charAt(i));
-            } else if (s.charAt(i) == ')') {
-                if (stk.isEmpty() || stk.peek() != '(') {
-                    return;
-                }
-                stk.pop();
-            }
-        }
-        if (stk.isEmpty()) {
-            System.out.println("1");
-        } else {
-            System.out.println("0");
-        }
     }
 }
