417e3655863f05439883303df83749f99351af3b
ChinmayKuJena <chinmay.jena7878@gmail.com>
2024-07-26 21:15:21 +0530

    OAuth remove all

--- travellerApp/src/main/java/com/traveller/travellerApp/Object/Data.java
+++
@@ -1,5 +0,0 @@
-package com.traveller.travellerApp.Object;
-
-public @interface Data {
-
-}
--- travellerApp/src/main/java/com/traveller/travellerApp/kafka/KafkaListener.java
+++
@@ -1,9 +0,0 @@
-package com.traveller.travellerApp.kafka;
-
-public @interface KafkaListener {
-
-    String topics();
-
-    String groupId();
-
-}
